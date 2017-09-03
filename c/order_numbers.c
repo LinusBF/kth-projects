ae02bbff5b5f622bede0995212c2c7f65c844a27
LinusBF <linus.webdevelopment@gmail.com>
2017-09-03 19:13:24 +0200

    Made order number program

---
+++ c/order_numbers.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int main(){
+    int number_1 = 5;
+    int number_2 = 1;
+    int number_3 = 2;
+
+    int min, max, mid;
+
+    if(number_1 > number_2){
+        max = number_1;
+        min = number_2;
+
+        if(number_3 > number_2){
+            if(number_3 > number_1){
+                max = number_3;
+            }
+        }
+        else {
+            mid = number_2;
+            min = number_3;
+        }
+    }
+    else{
+        max = number_2;
+        min = number_1;
+
+        if(number_3 > number_1){
+            if(number_3 > number_2){
+                max = number_3;
+            }
+        }
+        else {
+            mid = number_1;
+            min = number_3;
+        }
+    }
+
+    printf("%d is the largest number", &max);
+    printf("%d is the second largest number", &mid);
+    printf("%d is the smallest number", &min);
+}
