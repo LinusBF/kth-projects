379e224440a4d930edc0321cac8c33d4116bf13e
LinusBF <linus.webdevelopment@gmail.com>
2017-09-03 19:46:24 +0200

    Added insertion sort

--- c/order_numbers.c
+++ c/order_numbers.c
@@ -1,50 +1,81 @@
 #include <stdio.h>
 
-int main(){
-    int number_1 = 5;
-    int number_2 = 1;
-    int number_3 = 2;
-
+void basic_version(int n1, int n2, int n3){
     int min, max, mid;
 
-    if(number_1 > number_2){
-        max = number_1;
-        min = number_2;
+    if(n1 > n2){
+        max = n1;
+        min = n2;
 
-        if(number_3 > number_2){
-            if(number_3 > number_1){
-                max = number_3;
-                mid = number_1;
+        if(n3 > n2){
+            if(n3 > n1){
+                max = n3;
+                mid = n1;
             }
             else{
-                mid = number_3;
+                mid = n3;
             }
         }
         else {
-            mid = number_2;
-            min = number_3;
+            mid = n2;
+            min = n3;
         }
     }
     else{
-        max = number_2;
-        min = number_1;
+        max = n2;
+        min = n1;
 
-        if(number_3 > number_1){
-            if(number_3 > number_2){
-                max = number_3;
-                mid = number_2;
+        if(n3 > n1){
+            if(n3 > n2){
+                max = n3;
+                mid = n2;
             }
             else{
-                mid = number_3;
+                mid = n3;
             }
         }
         else {
-            mid = number_1;
-            min = number_3;
+            mid = n1;
+            min = n3;
         }
     }
+}
 
-    printf("%d is the largest number\n", max);
-    printf("%d is the second largest number\n", mid);
-    printf("%d is the smallest number\n", min);
+
+int* insertion_sort(int numbers[], int size){
+
+    for(int i = 1; i < size; i++){
+        int temp = numbers[i];
+        int j = i - 1;
+
+        while(j >= 0 && numbers[j] > temp){
+            numbers[j + 1] = numbers[j];
+            j--;
+        }
+
+        numbers[j + 1] = temp;
+    }
+
+    return numbers;
+}
+
+
+void print_array(int arr[], int n)
+{
+    int i;
+    for (i=0; i < n; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+
+
+int main(){
+    int nums[] = {10, 15, 25, 5, 2, 6, 2};
+
+    int size = sizeof(nums)/sizeof(nums[0]);
+    print_array(nums, size);
+
+    int *new_nums = insertion_sort(nums, size);
+
+    print_array(nums, size);
 }
