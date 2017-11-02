e2a8f274101c787f8dba9d30d1012dc9ea27106a
Linus Bein Fahlander <linus.webdevelopment@gmail.com>
2017-11-02 11:39:50 +0100

    Added input fields to answers and updated send answers function

--- js/extensions/dd_site_fix/fix_shit_up.js
+++ js/extensions/dd_site_fix/fix_shit_up.js
@@ -1,10 +1,11 @@
 /**
  * Created by Linus on 2017-11-01.
  */
-function answer_q(ex, q) {
+function answer_q(ex, q, a) {
     console.log("Sending answers for " + ex + ", " + q);
+    console.log(a);
 
-    //TODO - Add answers to argument and build ajax function to save answers.
+    //TODO - Build ajax function to save answers.
 }
 
 
@@ -20,11 +21,11 @@ window.onload = function () {
     var bundle = $("a[target='answers']").parent();
     bundle.addClass("header page-header");
 
-    $("table[border='2']").each(function (index) {
+    $("table[border='2']").each(function (index, element) {
         if(index < 1){
             return true;
         }
-        var parent = $(this).parent();
+        var parent = $(element).parent();
         parent.addClass("answers-wrapper");
 
         var exercise = parent.prevAll("h1")[0].innerHTML.split(",")[0].substr(-1, 1);
@@ -32,6 +33,16 @@ window.onload = function () {
         parent.append("<input type='hidden' name='exercise' value='" + exercise + "'>");
         parent.append("<input type='hidden' name='question' value='" + question + "'>");
         parent.append("<button class='btn btn-primary answer-trigger' >Skicka svar</button>");
+
+        var tr_answers = $(element).children("tbody").children("tr")[1];
+        $(tr_answers).addClass("answer-tr");
+
+        var td_answers = $(tr_answers).children("td");
+
+        $(td_answers).each(function (index, td_element) {
+            $(td_element)[0].innerHTML = "<input id='" + exercise + "_" + question + "_" + index + "' " +
+                "class='answer-input' type='text'>";
+        });
     });
 
     $(".answer-trigger").on("click", function (e) {
@@ -39,7 +50,14 @@ window.onload = function () {
 
         var ex = elem.siblings("input[name='exercise']").attr('value');
         var q = elem.siblings("input[name='question']").attr('value');
-        //TODO - Gather answer info
-        answer_q(ex, q);
+
+        var tr_answers = elem.siblings("table[border='2']").children("tbody").children(".answer-tr");
+        var td_answers = $(tr_answers).children("td");
+        var answers = [];
+
+        $(td_answers).each(function (index, td_element) {
+            answers.push([index, $(td_element).children(".answer-input").val()]);
+        });
+        answer_q(ex, q, answers);
     });
 };
