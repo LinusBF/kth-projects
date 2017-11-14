598ec108e025af77528ca59f176b08fef9d2b275
LinusBF <linus.webdevelopment@gmail.com>
2017-11-14 22:28:22 +0100

    Created file for OU3

---
+++ java/src/ou3/DenKortasteVagen.java
@@ -0,0 +1,21 @@
+package ou3;
+
+/**
+ * Created by Linus on 2017-11-14.
+ */
+public class DenKortasteVagen {
+        // mellanstationer returnerar en vektor med de mellanstationer som finns på den kortaste
+        // vägen. Ordningsnummer av den första mellanstationen finns på index 1, och ordningsnummer
+        // av den andra mellanstationen på index 2 i vektorn.
+        public static int[] mellanstationer (double[] a, double[][] b, double[] c)
+        {
+            // koden här
+            return new int[]{};
+        }
+        // langd returnerar längden av den kortaste vägen.
+        public static double langd (double[] a, double[][] b, double[] c)
+        {
+            // koden här
+            return 0;
+        }
+}
