20df1bc8b53a351780a79ae65e4fa06a549f557e
Linus KTH <linus.webdevelopment@gmail.com>
2018-01-25 22:51:53 +0100

    Finished work on seminar1

--- c/seminar1.c
+++ c/seminar1.c
@@ -31,22 +31,45 @@ int foo(int a){
     return t;
 }
 
+void dumb_shit(int x, int y, int *z){
+    *z = *z & ~(0xff) | ((x & (0x1f << 13)) >> 13) | ((y & 0x7) << 5);
+}
+
+void square_reverse(const double *x, double *y, const int len){
+    for (int i = 0; i < len; ++i)
+    {
+        double t = *(x + i);
+        *(y + ((len - 1) - i)) = t * t;
+    }
+}
+
 
 int main(int argc, char const *argv[])
 {
-    /*
-    signed int x = 123456789;
-    signed int y = 987654321;
-    signed int z = 192837465;
+
+    signed int x = 0x23FA5B6C;
+    signed int y = 0xFFFB9AEC;
+    signed int z = 0x34FF7E4A;
 
     printBits(sizeof(x), &x);
+    printBits(sizeof(y), &y);
     printBits(sizeof(z), &z);
 
-    z = z & (x >> 17 );
+    dumb_shit(x, y, &z);
 
     printBits(sizeof(z), &z);
-    */
-    int s = foo(7);
-    printf("%d\n", s);
+
+    printf("%d\n", foo(7));
+
+    double in[] = {11.0, 20.0, 100.0};
+    double out[3];
+
+    square_reverse(in, out, 3);
+
+    for (int i = 0; i < 3; ++i)
+    {
+        printf("%f\n", out[i]);
+    }
+
 	return 0;
 }
