cdd2f3ba9e2b0ce695bc4cf5da23ecf74a9961db
Linus Bein Fahlander <linus.webdevelopment@gmail.com>
2018-01-29 14:59:25 +0100

    Finished assignment 3

--- assembly/lab_1/timetemplate.asm
+++ assembly/lab_1/timetemplate.asm
@@ -74,3 +74,81 @@ tiend:	sw	$t0,0($a0)	# save updated result
 
   # you can write your code for subroutine "hexasc" below this line
   #
+
+hexasc:
+	andi $t0, $a0, 0xf	# Mask a0 so that only the 4 lsb are left
+	slti $t1, $t0, 10
+	beq $t1, $0, letter	# If a0 >= 10 handle like letter
+	nop			# Else handle like number
+	addi $t2, $t0, 0x30	# Add 0x30 to a0 to convert it to the ascii code for a0
+	j end			# Skip letter handling
+	nop			# Safety
+	letter:
+	addi $t0, $t0, -10	# Reduce t0 to account for the offset of 10 in the number for letter conversion
+	addi $t2, $t0, 0x41	# Add 0x41 to a0 to convert it to the ascii code for the letter a0 represents
+
+	end:
+	add $v0, $t2, $0	# Set v0 to the new converted number
+	jr $ra			# Return to function call
+	nop			# Safety
+	
+delay:
+ jr $ra
+ nop
+ 
+time2string:
+	andi $t0, $a1, 0xffff	# Mask a1 so that only the 4 LSB are left
+	li $t1, 0		# Init value to store answer
+	li $t2, 0		# Init i for loop
+	PUSH($a0)
+	PUSH($a1)
+	PUSH($t0)
+	PUSH($t1)
+	PUSH($t2)
+	loop:
+		slti $t3, $t2, 4	# Check i >= 4
+		beq $t3, $0, done
+		addi $t4, $0, 3
+ 		sub $t4, $t4, $t2	# Set t4 to 4 - i
+ 		sll $t4, $t4, 2
+ 		srlv $t5, $t0, $t4	# Shift a1 so that the the byte at i is visible
+ 		add $a0, $t5, $0	# Set argument for hexasc function
+ 		jal hexasc		# Call hexasc
+ 		nop
+ 		POP($t2)		# Load variabels after function call
+ 		POP($t1)
+ 		POP($t0)
+ 		POP($a1)
+ 		POP($a0)
+ 		sll $t1, $t1, 8
+ 		or $t1, $t1, $v0
+ 		addi $t2, $t2, 1
+ 		PUSH($a0)	# PUSH variabels for safe keeping during next loop
+		PUSH($a1)
+		PUSH($t0)
+		PUSH($t1)
+		PUSH($t2)
+		j loop
+		nop
+ 	done:
+ 		POP($t2)	# Load variabels after function call
+ 		POP($t1)
+ 		POP($t0)
+ 		POP($a1)
+ 		POP($a0)
+ 		
+ 		andi $t3, $t1, 0xffff0000
+ 		ori $t3, $t3, 0x3A00
+ 		andi $t4, $t1, 0xff00
+ 		srl $t4, $t4, 8
+ 		or $t3, $t3, $t4
+ 		
+ 		andi $t5, $t1, 0xff
+ 		sll $t5, $t5, 24
+ 		
+ 		sw $t3, 0($a0)
+ 		sw $t5, 4($a0)
+ 
+ 		jr $ra
+ 
+ 
