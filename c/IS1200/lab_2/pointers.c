0eacbe240bd67ccf7538a0c8296176f8dbae53b1
LinusBF <linus.webdevelopment@gmail.com>
2018-02-05 20:44:06 +0100

    Added comments and made list initializing more dynamic

--- c/IS1200/lab_2/pointers.c
+++ c/IS1200/lab_2/pointers.c
@@ -4,6 +4,7 @@
 
 char *text1 = "This is a string.";
 char *text2 = "Yet another thing.";
+// Initializing variables as described in assembly file - Linus Bein Fahlander
 int *list1;
 int *list2;
 int count = 0;
@@ -24,18 +25,20 @@ void endian_proof(const char *c) {
 
 }
 
+// Copies the chars in text to list in ascii format - Linus Bein Fahlander
 void copycodes(char *text, int *list, int *count) {
     do {
-        *list++ = (int) *text++;
-        (*count)++;
-    } while (*text != 0);
+        *list++ = (int) *text++; // Save the ascii value of "text" in "list" and increment both addresses
+        (*count)++; // Increment the count variable
+    } while (*text != 0); // Continue until the value at text is 0
 }
 
+// - Linus Bein Fahlander
 void work() {
-    list1 = calloc(20, sizeof(int));
+    list1 = calloc(80 / sizeof *list1, sizeof *list1); // Initialize list1, size is 80 bits as described in assembly file
     copycodes(text1, list1, &count);
 
-    list2 = calloc(20, sizeof(int));
+    list2 = calloc(80 / sizeof *list2, sizeof *list2); // Initialize list1, size is 80 bits as described in assembly file
     copycodes(text2, list2, &count);
 }
 
@@ -50,6 +53,6 @@ int main(void) {
 
     endian_proof((char *) &count);
 
-    free(list1);
-    free(list2);
+    free(list1); // - Linus Bein Fahlander
+    free(list2); // - Linus Bein Fahlander
 }
