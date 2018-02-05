a4efbfc272b8a987a7aa2cbbc18347afedc1b6bb
LinusBF <linus.webdevelopment@gmail.com>
2018-02-05 15:14:19 +0100

    Finished work for Lab 2 - Assignment 2

--- c/IS1200/lab_2/print-primes.c
+++ c/IS1200/lab_2/print-primes.c
@@ -10,36 +10,51 @@
 #include <stdlib.h>
 
 #define COLUMNS 6
+int written_numbers = 0;
 
 
-void print_primes(int n){
-  // Should print out all prime numbers less than 'n'
-  // with the following formatting. Note that
-  // the number of columns is stated in the define
-  // COLUMNS
+// Checks if a positive integer n is prime or not - Linus Bein Fahlander
+int is_prime(int n){
+    for(int i = 2; i*i <= n; i++){ // Check all numbers starting from 2 until i squared is greater than n
+        if(n%i == 0){ // If n % i is 0 then the number is not prime as it is divisible by i
+            return 0; // Return false
+        }
+    }
 
-  printf("%10d ", 2);
-  printf("%10d ", 3);
-  printf("%10d ", 5);
-  printf("%10d ", 7);
-  printf("%10d ", 11);
-  printf("%10d ", 13);
-  printf("\n");
-  printf("%10d ", 17);
-  printf("%10d ", 19);
+    return 1; // Return true
+}
 
-  printf("\n");
+// Prints a number in one of the columns - Linus Bein Fahlander
+void print_number(int n){
+    if(written_numbers == COLUMNS){
+        printf("\n");
+        written_numbers = 0;
+    }
+
+    printf("%10d ", n);
+    written_numbers++;
+}
+
+// Prints all prime numbers up to and including an integer n - Linus Bein Fahlander
+void print_primes(int n) {
+    int i = 2;
+    while(i <= n){
+        if(is_prime(i)){
+            print_number(i);
+        }
+        i++;
+    }
 }
 
 // 'argc' contains the number of program arguments, and
 // 'argv' is an array of char pointers, where each
 // char pointer points to a null-terminated string.
-int main(int argc, char *argv[]){
-  if(argc == 2)
-    print_primes(atoi(argv[1]));
-  else
-    printf("Please state an interger number.\n");
-  return 0;
+int main(int argc, char *argv[]) {
+    if (argc == 2)
+        print_primes(atoi(argv[1]));
+    else
+        printf("Please state an interger number.\n");
+    return 0;
 }
 
  
