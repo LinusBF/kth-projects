57380b2eccb217073036b901fdb887b80be0d174
LinusBF <linus.webdevelopment@gmail.com>
2018-09-09 19:38:49 +0200

    Started work on ID1020 LAB 1

---
+++ ID1020/lab1/backwards.c
@@ -0,0 +1,70 @@
+/*
+ Created by Linus on 2018-09-09.
+
+ Lab 1 - Assignment 1
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node {
+    char c;
+    struct node *prev;
+} node;
+
+/* RECURSIVE SOLUTION */
+
+node *recursiveIn(node *prev) {
+    char in = getchar();
+    if (in != '\n') {
+        node *newNode = malloc(sizeof(node));
+        newNode->c = in;
+        newNode->prev = prev;
+        return recursiveIn(newNode);
+    }
+
+    return prev;
+}
+
+void recursivePrint(node *lastIn) {
+    if (lastIn->prev != NULL) {
+        putchar(lastIn->c);
+        recursivePrint(lastIn->prev);
+    } else {
+        putchar(lastIn->c);
+        putchar('\n');
+    }
+    free(lastIn);
+}
+
+/* ITERATIVE SOLUTION */
+
+node *iterativeIn(node *prev) {
+    char in = getchar();
+    while (in != '\n') {
+        node *newNode = malloc(sizeof(node));
+        newNode->c = in;
+        newNode->prev = prev;
+        prev = newNode;
+        in = getchar();
+    }
+    return prev;
+}
+
+void iterativePrint(node *lastIn) {
+    while (lastIn != NULL) {
+        putchar(lastIn->c);
+        lastIn = lastIn->prev;
+    }
+    putchar('\n');
+}
+
+
+int main() {
+    node *recursiveInput = recursiveIn(NULL);
+    recursivePrint(recursiveInput);
+    node *iterateInput = iterativeIn(NULL);
+    iterativePrint(iterateInput);
+}
+
+
---
+++ ID1020/lab1/src/Reverse.java
@@ -0,0 +1,5 @@
+/**
+ * Created by Linus on 2018-09-09.
+ */
+public class Reverse {
+}
