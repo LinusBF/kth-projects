4cfbfd854e8ec2efd0a12db03ee4db699a7aa67e
Linus Bein Fahlander <linus.webdevelopment@gmail.com>
2018-09-09 20:10:50 +0200

    Updated project structure for Java support

--- ID1020/lab1/src/Reverse.java
+++
@@ -1,5 +0,0 @@
-/**
- * Created by Linus on 2018-09-09.
- */
-public class Reverse {
-}
---
+++ ID1020/src/lab1/Reverse.java
@@ -0,0 +1,10 @@
+package lab1;
+
+/**
+ * Created by Linus on 2018-09-09.
+ */
+public class Reverse {
+    public static void main(String[] args) {
+        System.out.println("Test");
+    }
+}
