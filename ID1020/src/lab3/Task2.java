0e557546bc755d11f634f6fe94dce010be2af9ec
LinusBF <linus.webdevelopment@gmail.com>
2018-10-01 00:03:51 +0200

    Added fixes for Lab 3 - Task 2

--- ID1020/src/lab3/Task2.java
+++ ID1020/src/lab3/Task2.java
@@ -9,19 +9,16 @@ import java.util.Scanner;
  */
 public class Task2 {
     public static void main(String[] args) throws FileNotFoundException {
-        Scanner scan =  new Scanner(new FileReader("C:\\Projects\\kth-projects\\ID1020\\src\\lab3\\filtered_text.txt"));
-        Scanner scan2 =  new Scanner(new FileReader("C:\\Projects\\kth-projects\\ID1020\\src\\lab3\\filtered_text.txt"));
+        Scanner scan =  new Scanner(new FileReader("E:\\kth-projects\\ID1020\\src\\lab3\\filtered_text.txt"));
+        Scanner scan2 =  new Scanner(new FileReader("E:\\kth-projects\\ID1020\\src\\lab3\\filtered_text.txt"));
 
         BinarySearchST<String, Integer> ST = new BinarySearchST<>();
         BST<String, Integer> BST = new BST<>();
 
-        long timestart1 = System.nanoTime();
-        FrequencyCounter.freqCount(ST, scan);
-        long endtime1 = System.nanoTime();
-        FrequencyCounter.freqCount(BST, scan2);
-        long endtime2 = System.nanoTime();
+        long exeTime1 = FrequencyCounter.freqCount(ST, scan);
+        long exeTime2 = FrequencyCounter.freqCount(BST, scan2);
 
-        System.out.println("Symbol Table: " + ((endtime1 - timestart1) / 1000000));
-        System.out.println("Binary Tree: " + ((endtime2 - endtime1) / 1000000));
+        System.out.println("Symbol Table: " + (exeTime1 / 1000000));
+        System.out.println("Binary Tree: " + (exeTime2 / 1000000));
     }
 }
