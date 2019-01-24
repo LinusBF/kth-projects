79b6e5c39104026421b747b7b6f4fd4ef0daa58f
LinusBF <linus.webdevelopment@gmail.com>
2019-01-24 08:21:18 +0100

    AAAAAAAAAAAAAAAAAAAAHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH

--- elixir/test.ex
+++ elixir/test.ex
@@ -5,6 +5,35 @@ defmodule Test do
     4 + 2
   end
 
+  def insert(x, l) do
+    case l do
+      [] ->
+        [x]
+      [h | t] when h < x ->
+        [h | insert(x, t)]
+      [h | t] ->
+        [x | [h | t]]
+    end
+  end
+
+  def isort(l) do
+    isort(l, [])
+  end
+
+  def isort(list, sorted) do
+    case sorted do
+      [] ->
+        [h | t] = list
+        isort(t, [h])
+      [h | t] when h < list ->
+        [h2 | t2] = list
+        isort(t2, insert(h2, sorted))
+      [h | t] ->
+        [h2 | t2] = list
+        [h2 | isort(t2, sorted)]
+    end
+  end
+
   def ack(m, n) do
     cond do
       m == 0 ->
@@ -15,5 +44,4 @@ defmodule Test do
         ack(m - 1, ack(m, n - 1))
     end
   end
-
 end
